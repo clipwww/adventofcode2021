758aef3caa25cf6f673a22236c49c8f55246631b
David, Jian <david.jian@myre.life>
2021-12-13 17:32:17 +0800

    init: day1 ~ day3

---
+++ README.md
@@ -0,0 +1,15 @@
+# Adventure Of Code 2021
+
+[Adventure Of Code 2021 Link](https://adventofcode.com/2021)
+
+- day1
+  - ✅ part1
+  - ✅ part2
+- day2
+  - ✅ part1
+  - ✅ part2
+- day3
+  - ✅ part1
+  - ✅ part2
+
+
---
+++ src/aocday.ts
@@ -0,0 +1,22 @@
+function day() {
+  console.time();
+
+  const puzzle = ``
+
+  function part1() {
+    console.log('part1')
+  }
+
+  part1();
+
+  function part2() {
+    console.log('part2')
+  }
+
+  
+  part2();
+
+  console.timeEnd();
+}
+
+day()
